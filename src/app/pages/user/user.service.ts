25fbfbcb900948d1eb77d0975463f2a1a59ddea1
seiftrad1 <msdt.trad@hotmail.com>
2019-07-07 17:42:49 +0100

    first contact between spring and angular

--- src/app/pages/user/user.service.ts
+++ src/app/pages/user/user.service.ts
@@ -1,11 +1,6 @@
 import {Injectable} from '@angular/core';
-import { HttpClient, HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from './user.model';
-
-const httpOptions = {
-   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-};
 
 @Injectable()
 export class UserService {
